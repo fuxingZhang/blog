a4ea2bcdd45ebb7d308e05510c499d43a3742946
fuxingZhang <502545703@qq.com>
2017-09-24 01:27:41 +0800

    'mysql'

--- models/db.js
+++ models/db.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
         idle: 30000
     },
     dialectOptions: {
-      socketPath: "/var/run/mysqld/mysqld.pid"
+      socketPath: "/var/lib/mysql/mysql.sock"
     },
     define: {
         paranoid: true
